41bb7be83afb8ec1ed51ee32ecc00851edf53f36
Trevor Adcock <trevor.adcock@mac.com>
2019-01-18 14:22:01 -0700

    Adds Master List View

---
+++ CloudKitJournal/ViewController/EntryListTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EntryListTableViewController.swift
+//  CloudKitJournal
+//
+//  Created by DevMountain on 1/18/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class EntryListTableViewController: UITableViewController {
+  
+  //MARK: - View Life Cycle Functions
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    EntryController.shared.fetchEntries { (success) in
+      if success{
+        self.tableView.reloadData()
+      }
+    }
+  }
+  
+  // MARK: - Table view data source
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return EntryController.shared.entries.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
+    let entry = EntryController.shared.entries[indexPath.row]
+    cell.textLabel?.text = entry.title
+    return cell
+  }
+  
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+  }
+}
